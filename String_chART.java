645005941433bba7e9f5315de8bbcf8aeaa31f57
NAVEEN-ADEPU <142499282+NAVEEN-ADEPU@users.noreply.github.com>
2024-05-01 20:55:24 +0530

    Add files via upload

---
+++ String_chART.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class String_chART {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// we can retrieve the string value by using an index number
+		 String level= "levitica";
+		 System.out.println(level.charAt(4)); // here 0=l, 1=e,2=v,3=i,4=t,5=i,6=c,7=a
+
+	}
+
+}
