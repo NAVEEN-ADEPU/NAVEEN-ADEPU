ee58e6daecaadd3083a5b16a9e8b6f9ce9b9261d
NAVEEN-ADEPU <142499282+NAVEEN-ADEPU@users.noreply.github.com>
2024-05-02 15:35:10 +0530

    Add files via upload

---
+++ Singledimensionalarray.java
@@ -0,0 +1,25 @@
+package myfirstselenium;
+
+public class Singledimensionalarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int arr[]=new int[7];
+    arr[0]=10;
+    arr[1]=20;
+    arr[3]=30;
+    arr[4]=40;
+    arr[5]=50;
+    arr[6]=60;
+    for(int i=0;i<arr.length;i++) {
+    	System.out.println(arr[i]);
+    	
+    }
+    	
+	}
+
+
+
+	}
+
+
